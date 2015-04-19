fa016a0dfb3c4b89f570bc4e466260c6aa74e266
Audra Andrew <aandrew@uta.edu>
2015-04-19 14:59:16 -0500

    stuff

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -3,10 +3,35 @@
 # aandrew@uta.edu
 # April 19th, 2015
 
-install.packages("dplyr", dependencies = TRUE)
+# install.packages("dplyr", dependencies = TRUE)
+
+# Load dplyr library into working environment
 library("dplyr")
 
 # Read in data as tab-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t", header = TRUE)
 
+gap.in %>%
+  filter(pop > 15000000) %>%
+  select(country, year, pop) %>%
+  group_by(country) %>%
+  summarize(min = min(pop))
 
+#Challenge
+#Calculate the mean population per continent across all years prior to 1990
+
+gap.in%>%
+  filter(year < 1990) %>%
+  select(continent, year, pop) %>%
+  group_by(continent, year) %>%
+  summarize(mean = mean(pop)) -> data.out
+  
+data(iris)
+attach(iris)
+install.packages("tidyr", dependencies=TRUE)
+
+library("tidyr")
+
+# Turn iris dataset from wide into long format
+iris.long <- gather(irix, "Measurement", "Value", 1:4)
+  
